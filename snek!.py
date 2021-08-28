862e028ef4f6ecda290850ddc9cf1864809517fa
unknown <simao.diasalmeida@bbcag.ch>
2021-08-28 18:20:47 +0200

    +update Menu screen, Username, update Apple, update Head

--- snek!.py
+++ snek!.py
@@ -24,30 +24,93 @@ white = (255, 255, 255)
 
 # Fonts
 fontSize = 100
-mainFont = pygame.font.SysFont("", fontSize)
-snekFont = pygame.font.SysFont("", 150)
-playFont = pygame.font.SysFont("", 50)
+mainFont = pygame.font.Font(None, fontSize)
+snekFont = pygame.font.Font(None, 150)
+
+# Startvariables
+username = ""
 
 # Functions
 def resetMainScreen():
-    surface.fill(backgroundColor)
-    surface.blit(mainFont.render("Welcome to:", True, (54, 57, 59)), (80, 120))
-    surface.blit(snekFont.render("snek!", True, (155, 120, 155)), (82, 202))
-    surface.blit(snekFont.render("snek!", True, (255, 220, 255)), (80, 200))
+    global username
 
-
-    active = False
-    text = ""
-    font2 = pygame.font.Font(None, 32)
-    input_box = pygame.Rect(100, 100, 140, 32)
-    color_inactive = pygame.Color("lightskyblue3")
-    color_active = pygame.Color("dodgerblue2")
+    font2 = pygame.font.Font(None, 50)
+    input_box = pygame.Rect(185, 600, 400, 50)
+    playBox = pygame.Rect(80, 600, 95, 50)
+    color_inactive = (100, 100, 100)
+    color_active = (196, 97, 99)
     color = color_inactive
-    while True: # https://stackoverflow.com/questions/46390231/how-can-i-create-a-text-input-box-with-pygame
+    active = False
+    usernameDone = False
+    highlightPlayButton = False
+
+    while True:
+        # Main Logo
         surface.fill(backgroundColor)
         surface.blit(mainFont.render("Welcome to:", True, (54, 57, 59)), (80, 120))
         surface.blit(snekFont.render("snek!", True, (155, 120, 155)), (82, 202))
         surface.blit(snekFont.render("snek!", True, (255, 220, 255)), (80, 200))
+
+
+        # Input Box
+        pygame.draw.rect(surface, (100, 100, 100), pygame.Rect(185, 600, 400, 50), border_radius=10)
+        pygame.draw.rect(surface, (255, 255, 255), pygame.Rect(188, 603, 394, 44), border_radius=10)
+
+        # Play Button Box
+        if usernameDone:
+            if username == "":
+                usernameInputColor = (200, 200, 200)
+                txt_surface = font2.render("Pls Username uwu", True, usernameInputColor)
+                input_box.w = 400
+                surface.blit(txt_surface, (input_box.x + 10, input_box.y + 10))
+                pygame.draw.rect(surface, color, input_box, 4, border_radius=10)
+            else:
+                usernameInputColor = (54, 57, 59)
+                txt_surface = font2.render(username, True, usernameInputColor)
+                input_box.w = 400
+                surface.blit(txt_surface, (input_box.x + 10, input_box.y + 10))
+                pygame.draw.rect(surface, color, input_box, 4, border_radius=10)
+            pygame.draw.rect(surface, (100, 100, 100), pygame.Rect(80, 600, 95, 50), border_radius=10)
+            playFont = pygame.font.Font(None, 50)
+            surface.blit(playFont.render("Play", True, (255, 255, 255)), (90, 608))
+            pygame.display.flip()
+            time.sleep(0.3)
+            pygame.draw.rect(surface, (50, 50, 50), pygame.Rect(80, 600, 95, 50), border_radius=10)
+            pygame.draw.rect(surface, (100, 100, 100), pygame.Rect(76, 596, 95, 50), border_radius=10)
+            playFont = pygame.font.Font(None, 52)
+            surface.blit(playFont.render("Play", True, (255, 255, 255)), (89, 607))
+            pygame.display.flip()
+            time.sleep(0.1)
+
+            if username != "":
+                return
+            else:
+                usernameDone = False
+
+
+        pygame.draw.rect(surface, (50, 50, 50), pygame.Rect(80, 600, 95, 50), border_radius=10)
+        pygame.draw.rect(surface, (100, 100, 100), pygame.Rect(76, 596, 95, 50), border_radius=10)
+        playFont = pygame.font.Font(None, 52)
+        if highlightPlayButton:
+            surface.blit(playFont.render("Play", True, (255, 220, 255)), (89, 607))
+        else:
+            surface.blit(playFont.render("Play", True, (255, 255, 255)), (89, 607))
+
+        mouse = pygame.mouse.get_pos()
+        if playBox.collidepoint(mouse) and not usernameDone:
+            highlightPlayButton = True
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    usernameDone = True
+        else:
+            highlightPlayButton = False
+
+
+        # Input Box -> https://stackoverflow.com/questions/46390231/how-can-i-create-a-text-input-box-with-pygame
+        if input_box.collidepoint(mouse):
+            color = (255, 220, 255)
+        else:
+            color = color_active if active else color_inactive
 
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
@@ -55,34 +118,31 @@ def resetMainScreen():
                     active = not active
                 else:
                     active = False
-                color = color_active if active else color_inactive
             if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    usernameDone = True
                 if active:
-                    if event.key == pygame.K_RETURN:
-                        print(text)
-                        text = ""
-                    elif event.key == pygame.K_BACKSPACE:
-                        text = text[:-1]
-                    else:
-                        text += event.unicode
-        txt_surface = font2.render(text, True, color)
+                    if event.key == pygame.K_BACKSPACE:
+                        username = username[:-1]
+                    elif event.key == pygame.K_DOLLAR:
+                        username = ""
+                    elif txt_surface.get_width() <= 350 and event.key != pygame.K_RETURN:
+                        username += event.unicode
+        if username == "" and not active:
+            usernameInputColor = (200, 200, 200)
+            txt_surface = font2.render("Username", True, usernameInputColor)
+        else:
+            usernameInputColor = (54, 57, 59)
+            txt_surface = font2.render(username, True, usernameInputColor)
+        input_box.w = 400
+        surface.blit(txt_surface, (input_box.x+10, input_box.y+10))
 
-        width = max(200, txt_surface.get_width()+10)
-        input_box.w = width
 
-        surface.blit(txt_surface, (input_box.x+5, input_box.y+5))
-        pygame.draw.rect(surface, color, input_box, 2)
+        pygame.draw.rect(surface, color, input_box, 4, border_radius=10)
 
 
-        pygame.draw.rect(surface, (100, 100, 100), pygame.Rect(185, 600, 400, 50), border_radius=10)
-        pygame.draw.rect(surface, (255, 255, 255), pygame.Rect(188, 603, 394, 44), border_radius=10)
-
-        pygame.draw.rect(surface, (100, 100, 100), pygame.Rect(80, 600, 95, 50), border_radius=10)
-        font = pygame.font.SysFont("Uni Sans", 50)
-        surface.blit(playFont.render("Play", True, (255, 255, 255)), (90, 608))
 
         pygame.display.flip()
-
 
 
 
@@ -95,7 +155,6 @@ def showScore():
         surface.blit(fontBigScore.render("Score: " + str(score), True, (54, 57, 59)), (10+i*0.48, 10+i*0.38))
         pygame.display.flip()
 
-    font = pygame.font.SysFont("Uni Sans", 50)
     while True:
         surface.fill(backgroundColor)
         mouse = pygame.mouse.get_pos()
@@ -136,12 +195,15 @@ def resetBackground():
     # Score
     surface.blit(font.render("Score: " + str(score), True, (54, 57, 59)), (10, 10))
 
-    #pygame.display.flip()
+    # Username
+    usernameSurface = font.render(username, True, (54, 57, 59))
+    width = max(0, usernameSurface.get_width() + 10)
+    surface.blit(usernameSurface, (840-width, 10))
 
 
 def drawSnake():
     # Snake Head
-    pygame.draw.rect(surface, snakeColor, ((snakeHeadX * 50 + 1), (snakeHeadY * 50 + 51), 50, 50))
+    #pygame.draw.rect(surface, snakeColor, ((snakeHeadX * 50 + 1), (snakeHeadY * 50 + 51), 50, 50))
 
     # Snake Body
     for i in reversed(range(len(snakeTailX))):
@@ -164,21 +226,39 @@ def drawSnake():
 
     # "Eyes"
     if direction == "right":
-        pygame.draw.rect(surface, white, ((snakeHeadX * 50 + 45), (snakeHeadY * 50 + 74), 5, 5))
+        pygame.draw.rect(surface, snakeColor, pygame.Rect((snakeHeadX * 50 + 1), (snakeHeadY * 50 + 51), 50, 50), border_top_right_radius=25, border_bottom_right_radius=25)
     if direction == "left":
-        pygame.draw.rect(surface, white, ((snakeHeadX * 50 + 1), (snakeHeadY * 50 + 74), 5, 5))
+        pygame.draw.rect(surface, snakeColor, pygame.Rect((snakeHeadX * 50 + 1), (snakeHeadY * 50 + 51), 50, 50), border_top_left_radius=25, border_bottom_left_radius=25)
     if direction == "up":
-        pygame.draw.rect(surface, white, ((snakeHeadX * 50 + 23), (snakeHeadY * 50 + 51), 5, 5))
+        pygame.draw.rect(surface, snakeColor, pygame.Rect((snakeHeadX * 50 + 1), (snakeHeadY * 50 + 51), 50, 50), border_top_right_radius=25, border_top_left_radius=25)
     if direction == "down":
-        pygame.draw.rect(surface, white, ((snakeHeadX * 50 + 23), (snakeHeadY * 50 + 95), 5, 5))
+        pygame.draw.rect(surface, snakeColor, pygame.Rect((snakeHeadX * 50 + 1), (snakeHeadY * 50 + 51), 50, 50), border_bottom_right_radius=25, border_bottom_left_radius=25)
 
     #pygame.display.flip()
 
 
 def drawApple():
+    colorApple = (255, 0, 0)
+    colorStem = (134, 89, 45)
+    colorLeaf = (92, 214, 92)
+    colorLeafMiddle = (32, 154, 32)
+    colorHighlightApple = (255, 255, 255)
+
     for i in range(numApplesWanted):
         if applesX[i] != "":
-            pygame.draw.circle(surface, appleColor, (applesX[i] * 50 + 25, applesY[i] * 50 + 75), 23)
+            pygame.draw.rect(surface, colorStem, pygame.Rect((applesX[i]*50+25) - 1, (applesY[i]*50+75) - 18, 2, 10),border_radius=1)
+            pygame.draw.circle(surface, colorApple, ((applesX[i]*50+25), (applesY[i]*50+75) + 6), 16)
+            pygame.draw.ellipse(surface, colorLeaf, pygame.Rect((applesX[i]*50+25) + 3, (applesY[i]*50+75) - 20, 10, 6))
+            pygame.draw.rect(surface, colorLeafMiddle, pygame.Rect((applesX[i]*50+25) + 5, (applesY[i]*50+75) - 18, 6, 1))
+            pygame.draw.polygon(surface, colorHighlightApple, (((applesX[i]*50+25) - 7, (applesY[i]*50+75) - 6),
+                                                               ((applesX[i]*50+25) - 5, (applesY[i]*50+75) - 7),
+                                                               ((applesX[i]*50+25) - 3, (applesY[i]*50+75) - 6),
+                                                               ((applesX[i]*50+25) - 4, (applesY[i]*50+75) - 4),
+                                                               ((applesX[i]*50+25) - 7, (applesY[i]*50+75) - 1),
+                                                               ((applesX[i]*50+25) - 10, (applesY[i]*50+75) - 2),
+                                                               ((applesX[i]*50+25) - 9, (applesY[i]*50+75) - 1),
+                                                               ((applesX[i]*50+25) - 9, (applesY[i]*50+75) - 4),
+                                                               ))
 
 
 def moveSnakeForward():
@@ -258,22 +338,22 @@ while True:
 
             if idk:
                 keys = pygame.key.get_pressed()
-                if keys[K_LEFT]:
+                if keys[K_LEFT] or keys[K_a]:
                     if direction != "right":
                         direction = "left"
                         idk = False
 
-                elif keys[K_RIGHT]:
+                elif keys[K_RIGHT] or keys[K_d]:
                     if direction != "left":
                         direction = "right"
                         idk = False
 
-                elif keys[K_UP]:
+                elif keys[K_UP] or keys[K_w]:
                     if direction != "down":
                         direction = "up"
                         idk = False
 
-                elif keys[K_DOWN]:
+                elif keys[K_DOWN] or keys[K_s]:
                     if direction != "up":
                         direction = "down"
                         idk = False
@@ -321,7 +401,7 @@ while True:
             drawSnake()
             # Timer (https://www.programiz.com/python-programming/time)
             elapsedTime = time.time() - startTime
-            if elapsedTime > 0.3:
+            if elapsedTime > 0.5:
                 idk = True
                 startTime = time.time()
 
